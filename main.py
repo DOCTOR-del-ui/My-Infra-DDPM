497f1fd990a1c20aa9bc1c742c7e751d73dffcaf
lisonglin <123434@ww.com>
2024-03-05 14:00:28 +0800

    ckpt

---
+++ main.py
@@ -0,0 +1,172 @@
+import copy
+import json
+import os
+import warnings
+from absl import app, flags
+
+import torch
+from tensorboardX import SummaryWriter
+from torchvision.datasets import ImageFolder
+from torchvision.utils import make_grid, save_image
+from torchvision import transforms
+from tqdm import trange
+
+from diffusion import GaussianDiffusionTrainer, GaussianDiffusionSampler
+from model import UNet
+
+
+FLAGS = flags.FLAGS
+
+
+device = torch.device('cuda:0')
+
+def infiniteloop(dataloader):
+    while True:
+        for x, y in iter(dataloader):
+            yield x
+
+
+def warmup_lr(step):
+    return min(step, FLAGS.warmup) / FLAGS.warmup
+
+
+def evaluate(sampler, model):
+    model.eval()
+    with torch.no_grad():
+        images = []
+        desc = "generating images"
+        for i in trange(0, FLAGS.num_images, FLAGS.batch_size, desc=desc):
+            batch_size = min(FLAGS.batch_size, FLAGS.num_images - i)
+            x_T = torch.randn((batch_size, 3, FLAGS.img_size, FLAGS.img_size))
+            batch_images = sampler(x_T.to(device)).cpu()
+            images.append(batch_images)
+        images = torch.cat(images, dim=0)
+    model.train()
+
+    return images
+
+
+def train(): 
+    # dataset
+    dataset = ImageFolder(
+        root='./dataset',
+        transform=transforms.Compose([
+            transforms.Resize((FLAGS.img_size,FLAGS.img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]))
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=FLAGS.batch_size, shuffle=True,
+        num_workers=FLAGS.num_workers, drop_last=True)
+    datalooper = infiniteloop(dataloader)
+
+    # model setup
+    net_model = UNet(
+        T=FLAGS.T, ch=FLAGS.ch, ch_mult=FLAGS.ch_mult, attn=FLAGS.attn,
+        num_res_blocks=FLAGS.num_res_blocks, dropout=FLAGS.dropout)
+
+    optim = torch.optim.Adam(net_model.parameters(), lr=FLAGS.lr)
+    sched = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=warmup_lr)
+    trainer = GaussianDiffusionTrainer(
+        net_model, FLAGS.beta_1, FLAGS.beta_T, FLAGS.T).to(device)
+    net_sampler = GaussianDiffusionSampler(
+        net_model, FLAGS.beta_1, FLAGS.beta_T, FLAGS.T, FLAGS.img_size,
+        FLAGS.mean_type, FLAGS.var_type).to(device)
+
+    if FLAGS.parallel:
+        trainer = torch.nn.DataParallel(trainer)
+        net_sampler = torch.nn.DataParallel(net_sampler)
+
+    # log setup
+    os.makedirs(os.path.join(FLAGS.logdir, 'sample'))
+    x_T = torch.randn(FLAGS.sample_size, 3, FLAGS.img_size, FLAGS.img_size)
+    x_T = x_T.to(device)
+    grid = (make_grid(next(iter(dataloader))[0][:FLAGS.sample_size]) + 1) / 2
+    writer = SummaryWriter(FLAGS.logdir)
+    writer.add_image('real_sample', grid)
+    writer.flush()
+    # backup all arguments
+    with open(os.path.join(FLAGS.logdir, "flagfile.txt"), 'w') as f:
+        f.write(FLAGS.flags_into_string())
+    # show model size
+    model_size = 0
+    for param in net_model.parameters():
+        model_size += param.data.nelement()
+    print('Model params: %.2f M' % (model_size / 1000 / 1000))
+
+    # start training
+    with trange(FLAGS.total_steps, dynamic_ncols=True) as pbar:
+        for step in pbar:
+            # train
+            optim.zero_grad()
+            x_0 = next(datalooper).to(device)
+            loss = trainer(x_0).mean()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(
+                net_model.parameters(), FLAGS.grad_clip)
+            optim.step()
+            sched.step()
+
+            # log
+            writer.add_scalar('loss', loss, step)
+            pbar.set_postfix(loss='%.3f' % loss)
+
+            # sample
+            if FLAGS.sample_step > 0 and step % FLAGS.sample_step == 0:
+                net_model.eval()
+                with torch.no_grad():
+                    x_0 = net_sampler(x_T)
+                    grid = (make_grid(x_0) + 1) / 2
+                    path = os.path.join(
+                        FLAGS.logdir, 'sample', '%d.png' % step)
+                    save_image(grid, path)
+                    writer.add_image('sample', grid, step)
+                net_model.train()
+
+            # save
+            if FLAGS.save_step > 0 and step % FLAGS.save_step == 0:
+                ckpt = {
+                    'net_model': net_model.state_dict(),
+                    'sched': sched.state_dict(),
+                    'optim': optim.state_dict(),
+                    'step': step,
+                    'x_T': x_T,
+                }
+                torch.save(ckpt, os.path.join(FLAGS.logdir, 'ckpt_{}.pt'.format(step)))
+
+    writer.close()
+
+
+def eval():
+    # model setup
+    model = UNet(
+        T=FLAGS.T, ch=FLAGS.ch, ch_mult=FLAGS.ch_mult, attn=FLAGS.attn,
+        num_res_blocks=FLAGS.num_res_blocks, dropout=FLAGS.dropout)
+    sampler = GaussianDiffusionSampler(
+        model, FLAGS.beta_1, FLAGS.beta_T, FLAGS.T, img_size=FLAGS.img_size,
+        mean_type=FLAGS.mean_type, var_type=FLAGS.var_type).to(device)
+    if FLAGS.parallel:
+        sampler = torch.nn.DataParallel(sampler)
+        
+    # load model and evaluate
+    ckpt = torch.load(os.path.join(FLAGS.logdir, 'ckpt_{}.pt'.format(FLAGS.ckpt_index)))
+    model.load_state_dict(ckpt['net_model'])
+    samples = evaluate(sampler, model)
+    return samples
+    print(sampler.shape)
+"""     save_image(
+        torch.tensor(samples[:256]),
+        os.path.join(FLAGS.logdir, 'samples.png'),
+        nrow=16) """
+
+
+def main():
+    # suppress annoying inception_v3 initialization warning
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    if FLAGS.train:
+        train()
+    if FLAGS.eval:
+        return eval()
+
+
+
